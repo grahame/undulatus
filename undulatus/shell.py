cac3948fe3de4f014089856d88b595b23fe1489e
Grahame Bowland <grahame@angrygoats.net>
2011-02-21 00:37:23 +0800

    add README, LICENSE files (and splash warning)

--- undulatus/shell.py
+++ undulatus/shell.py
@@ -590,6 +590,22 @@ if __name__ == '__main__':
     delims = ''.join(filter(lambda x: x != '/', delims))
     readline.set_completer_delims(delims)
 
+    print """\
+Undulatus
+Copyright (C) 2011 Grahame Bowland <grahame@angrygoats.net>
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+See the file 'LICENSE' included with this software for more detail.
+"""
+
+
     while True:
             readline.redisplay()
             line = get_line(">> ")
