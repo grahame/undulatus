0256267ef2bfd32bf8fcecc3324bef11a4025974
Grahame Bowland <grahame@angrygoats.net>
2012-03-19 20:19:10 +0800

    fix up a bunch of standing tweetdb bugs

--- tracker.py
+++ tracker.py
@@ -11,17 +11,17 @@ class TweetTracker(threading.Thread):
     def __init__(self, twitter, db):
         self.twitter = twitter
         self.db = db
-        self.last_id = 0
-        self.base = len(self.tbl)
         self.key_to_tweet = {}
-        self.twitter_to_key = {}
+        self.tweet_to_key = {}
+        self.last_idx = 0
+        self.base = len(self.tbl)
         self.seen_users = set()
         self.load_recent()
 
     def load_recent(self):
         sys.stdout.write("loading recent tweets from database... ")
         sys.stdout.flush()
-        for tweet in self.db.get_recent(300):
+        for tweet in self.db.get_recent(500):
             self.cache_tweet(tweet)
         sys.stdout.write("done! %d tweets loaded.\n" % (len(self.key_to_tweet)))
         sys.stdout.flush()
@@ -74,13 +74,20 @@ class TweetTracker(threading.Thread):
         if key is not None:
             return key
         # calculate our 'A9' style key
-        key = self.make_key(self.last_id)
+        key = self.make_key(self.last_idx)
+        # remove any old points for that key
+        try:
+            old_tweet = self.key_to_tweet.pop(key)
+            twitter_id = old_tweet['id_str']
+            self.tweet_to_key.pop(twitter_id)
+        except KeyError:
+            pass
         # update
-        self.last_id = (self.last_id + 1) % (self.base * self.base)
+        self.last_idx = (self.last_idx + 1) % (self.base * self.base)
         # copy ourself in
         twitter_id = tweet['id']
         self.key_to_tweet[key] = tweet
-        self.twitter_to_key[twitter_id] = key
+        self.tweet_to_key[twitter_id] = key
         text = tweet_text(tweet)
         for username in get_usernames(text):
             self.seen_users.add(username)
@@ -91,7 +98,7 @@ class TweetTracker(threading.Thread):
         return self.key_to_tweet.get(key, None)
 
     def get_key_for_tweet(self, tweet):
-        return self.twitter_to_key.get(tweet['id'], None)
+        return self.tweet_to_key.get(tweet['id'], None)
 
     def print_tweet(self, tweet, suffix=''):
         screen_name = "%-15s" % (tweet['user']['screen_name'])
