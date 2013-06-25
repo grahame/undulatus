9c1113ffc45704f4ce31f60108aaea8959f87598
Grahame Bowland <grahame@angrygoats.net>
2013-06-25 14:34:34 +0000

    remove byhashtag, not used

--- undulatus.js
+++ undulatus.js
@@ -1,6 +1,6 @@
 {
   "_id" : "_design/undulatus",
-  "revision" : "6",
+  "revision" : "7",
   "views" : {
     "replies" : {
       "map" : "function(doc) { if (doc.in_reply_to_status_id_str) { emit(doc.in_reply_to_status_id_str, null); } }"
@@ -10,9 +10,6 @@
     },
     "byid" : {
         "map" : "function(doc) { if (doc.id) { emit(doc.id, null); } }"
-    },
-    "byhashtag" : {
-        "map" : "function(doc) { var match = function(s) { var r = /(^|\\s)#([^\\s]+)/; var match; while (match = s.match(r)) { s = s.substr(match.index + match[0].length); var hashtag = match[2]; emit(hashtag, null); } }; if (doc['text']) { match(doc['text']); } }"
     }
   }
 }
